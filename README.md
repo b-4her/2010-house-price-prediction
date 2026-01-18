f51532638ae23d93dddf0f9d294b60ae02fd5b29
b-4her <bahercsjourney@gmail.com>
2026-01-18 14:04:01 -0600

    Added split script and made the splits for our model

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ## Dataset
 [House Prices](https://www.kaggle.com/competitions/house-prices-advanced-regression-techniques/overview)
 
+IMPORTANT NOTE: you don't need to mention the project for the prof rn, just keep working on it and after they post some stuff you can share your result with them and ask for initial feedback to complete everything in the reading week.
+
+UPDATE the readme and make it similar to your github ones, you already wasted a lot of time on sturing them, so just use your effort.
---
+++ scripts/train_test_split.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def split_and_save(
+    input_csv: str,
+    output_dir: str = "data/split",
+    test_size: float = 0.2,
+    random_state: int = 42
+):
+    df = pd.read_csv(input_csv)
+
+    train_df, test_df = train_test_split(
+        df,
+        test_size=test_size,
+        shuffle=True,
+        random_state=random_state
+    )
+
+    train_df.to_csv(f"{output_dir}/train.csv", index=False)
+    test_df.to_csv(f"{output_dir}/test.csv", index=False)
+
+    print(f"Train rows: {len(train_df)}")
+    print(f"Test rows: {len(test_df)}")
+
+
+def main():
+    split_and_save("./data/data.csv")
+
+
+if __name__ == "__main__":
+    main()
